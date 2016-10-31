265e3e87261cd8fd58383f06283995d6205901f5
eslamadel <eslam.adel.mahmoud.ali@gmail.com>
2016-10-31 11:45:54 +0200

    Working VOI but not understanded

--- mandible-segmentation/src/apps/develop/extractSubVol.cpp
+++ mandible-segmentation/src/apps/develop/extractSubVol.cpp
@@ -13,14 +13,26 @@ int main(int argc , char ** argv)
     volRenderer->loadData(argv[1]);
     volRenderer->rescaleData(volRenderer->getScale(),
                              volRenderer->getShift());
-    vtkSmartPointer<vtkExtractVOI> extractor = vtkSmartPointer<vtkExtractVOI>::New();
+    vtkExtractVOI* extractor = vtkExtractVOI::New();
     extractor->SetInputConnection(volRenderer->getShifter()->GetOutputPort());
-//    extractor->SetVOI(0, 100, 0, 100, 0, 100);
-//    extractor->SetSampleRate(1, 1, 1);
-//    extractor->ReleaseDataFlagOff();
-//    extractor->Update();
+    extractor->SetVOI(0, 511, 0, 511, 50, 100);
+    extractor->SetSampleRate(1, 1, 1);
+    extractor->Update();
 
-    volRenderer->extractSurfaces(0, extractor->GetOutput());
+
+    vtkImageData* shifted = volRenderer->getShifter()->GetOutput();
+     std::cout << shifted->GetScalarComponentAsFloat(100, 50, 80, 0) << std::endl;
+
+    vtkImageData* extracted = extractor->GetOutput();
+    int* extractedDims = extracted->GetDimensions();
+     std::cout << "Dims: " << " x: " << extractedDims[0]
+                           << " y: " << extractedDims[1]
+                           << " z: " << extractedDims[2] << std::endl;
+     std::cout << "Number of points: " << extracted->GetNumberOfPoints() << std::endl;
+     std::cout << "Number of cells: " << extracted->GetNumberOfCells() << std::endl;
+
+     std::cout << extracted->GetScalarComponentAsDouble(200, 100, 50, 0) << std::endl;
+    volRenderer->extractSurfaces(1200, extractor->GetOutput());
 
 
     return EXIT_SUCCESS;
