581a028606fdf5c9044e7b3c1186c4986e981e69
eslamadel <eslam.adel.mahmoud.ali@gmail.com>
2016-11-10 17:12:14 +0200

    Documentation and enhacement

--- mandible-segmentation/src/filters/Segmentation.cpp
+++ mandible-segmentation/src/filters/Segmentation.cpp
@@ -2,14 +2,23 @@
 
 Segmentation::Segmentation(vtkSmartPointer<vtkImageData> input):inputData_(input)
 {
+
+    LOG_DEBUG("initialize Segmentation");
+    //Get input Data Dimentions
     dimentions_ = inputData_->GetDimensions();
+
+    //Get Extent (Rage of data that extracted from the original Volume)
     extent_ = inputData_->GetExtent();
+
+    //Initialization
     currentId_ = 0;
     count_ = 0;
     outputData_ = vtkSmartPointer<vtkImageData>::New();
     segmentsList_ = new QList<Segment>;
     initializeLabelMap_();
     createOutputData_();
+
+    //Run segmentation Algorithm
     startLabeling_();
 }
 
@@ -20,10 +29,10 @@ vtkSmartPointer<vtkImageData> Segmentation::getSegmentedData()
 
 void Segmentation::createOutputData_()
 {
-    LOG_DEBUG("Creating Image Data");
+    LOG_DEBUG("Creating Image Data of Output Volume");
     outputData_->SetDimensions(dimentions_[0], dimentions_[1], dimentions_[2]);
-    outputData_->SetScalarType(VTK_UNSIGNED_SHORT, inputData_->GetInformation());
-    outputData_->SetNumberOfScalarComponents(1, inputData_->GetInformation());
+    outputData_->SetScalarType(VTK_UNSIGNED_SHORT, outputData_->GetInformation());
+    outputData_->SetNumberOfScalarComponents(1, outputData_->GetInformation());
     outputData_->SetExtent(extent_[0], extent_[1],
             extent_[2], extent_[3],
             extent_[4], extent_[5]);
@@ -32,14 +41,14 @@ void Segmentation::createOutputData_()
 
 void Segmentation::initializeLabelMap_()
 {
+    LOG_DEBUG("Initialize the points -> segment map");
     for(int k = extent_[4] ; k <= extent_[5]; k++)
     {
         for(int j = extent_[2] ; j <= extent_[3]; j++)
         {
             for(int i = extent_[0] ; i <= extent_[1]; i++)
             {
-                double data = inputData_->GetScalarComponentAsDouble(i, j, k, 0);
-                if(data > 0)
+                if(inputData_->GetScalarComponentAsDouble(i, j, k, 0) > 0)
                 {
                     int ijk[3] = {i, j,k};
                     pointsLabels_[inputData_->ComputePointId(ijk)] = -1;
@@ -50,14 +59,14 @@ void Segmentation::initializeLabelMap_()
 }
 void Segmentation::startLabeling_()
 {
+    LOG_DEBUG("Start Labeling");
     for(int k = extent_[4] + 1; k <= extent_[5]; k++)
     {
         for(int j = extent_[2] + 1; j < extent_[3]; j++)
         {
             for(int i = extent_[0] + 1; i < extent_[1]; i++)
             {
-                double data = inputData_->GetScalarComponentAsDouble(i, j, k, 0);
-                if(data > 0)
+                if(inputData_->GetScalarComponentAsDouble(i, j, k, 0) > 0)
                     setLabel_(i, j, k);
             }
         }
@@ -69,29 +78,21 @@ void Segmentation::startLabeling_()
 
 void Segmentation::setLabel_(int i, int j, int k)
 {
-    QList<vtkIdType> neighbours = getNeighbours_(i, j, k);
+    QSet<int> neighbourSegments = getNeighbours_(i, j, k);
     int ijk[3] = {i, j, k};
-    QSet<int> segmentsIds;
-    for(auto it : neighbours)
-    {
-        if(pointsLabels_[it] >= 0)
-        {
-            segmentsIds.insert(pointsLabels_[it]);
-        }
-    }
-    if(segmentsIds.empty())
+    if(neighbourSegments.empty())
     {
         pointsLabels_[inputData_->ComputePointId(ijk)] = createSegment();
     }
     else
     {
-        pointsLabels_[inputData_->ComputePointId(ijk)] = updateSegmentsList(segmentsIds);
+        pointsLabels_[inputData_->ComputePointId(ijk)] = updateSegmentsList(neighbourSegments);
     }
 }
 
-QList<vtkIdType> Segmentation::getNeighbours_(int i, int j, int k)
+QSet<int> Segmentation::getNeighbours_(int i, int j, int k)
 {
-    QList<vtkIdType> neighbours;
+    QSet<int> segmentsIds;
     int ijk[3];
     for(int x = -1; x < 2; x++ )
     {
@@ -105,23 +106,32 @@ QList<vtkIdType> Segmentation::getNeighbours_(int i, int j, int k)
                     (x == -1 && y == 1))
             {
                 ijk[2] = k;
-                double data = inputData_->GetScalarComponentAsDouble(ijk[0], ijk[1], ijk[2], 0);
-                if(data > 0)
-                    neighbours.push_back(inputData_->ComputePointId(ijk));
+                if(inputData_->GetScalarComponentAsDouble(ijk[0], ijk[1], ijk[2], 0) > 0)
+                {
+                    if(pointsLabels_[inputData_->ComputePointId(ijk)] >= 0)
+                    {
+                        segmentsIds.insert(pointsLabels_[inputData_->ComputePointId(ijk)]);
+                    }
+                }
             }
             ijk[0] = i + x;
             ijk[1] = j + y;
             ijk[2] = k - 1;
-            double data = inputData_->GetScalarComponentAsDouble(ijk[0], ijk[1], ijk[2], 0);
-            if(data > 0)
-                neighbours.push_back(inputData_->ComputePointId(ijk));
+            if(inputData_->GetScalarComponentAsDouble(ijk[0], ijk[1], ijk[2], 0) > 0)
+            {
+                if(pointsLabels_[inputData_->ComputePointId(ijk)] >= 0)
+                {
+                    segmentsIds.insert(pointsLabels_[inputData_->ComputePointId(ijk)]);
+                }
+            }
         }
     }
-    return neighbours;
+    return segmentsIds;
 }
 
 int Segmentation::createSegment()
 {
+    LOG_DEBUG("Creating a new Segment with id = %d", currentId_);
     Segment newSegment;
     newSegment.id_ = currentId_;
     newSegment.pointsCount_ = 1;
@@ -161,9 +171,9 @@ int Segmentation::updateSegmentsList(QSet<int> segmentsIds)
 
 Segment Segmentation::getLargestSegment_()
 {
-    int listSize = segmentsList_->size();
-    double maxCount = segmentsList_->at(listSize - 1).pointsCount_;
-    int largestSegmentId = listSize - 1;
+    LOG_DEBUG("Getting The Largest Segment");
+    double maxCount = segmentsList_->at(segmentsList_->size() - 1).pointsCount_;
+    int largestSegmentId = segmentsList_->size() - 1;
     for(auto it = segmentsList_->end() - 1; it != segmentsList_->begin(); --it)
     {
         it->totalCount_ = it->pointsCount_;
@@ -175,11 +185,8 @@ Segment Segmentation::getLargestSegment_()
                 it->totalCount_ += segmentsList_->at(it3).pointsCount_;
             }
         }
-        double countVal = it->totalCount_;
-        //        if(it->totalCount_ > maxCount)
-        if(countVal > maxCount)
+        if(it->totalCount_ > maxCount)
         {
-            //            maxCount = it->totalCount_;
             maxCount = it->totalCount_;
             largestSegmentId = it->id_;
         }
@@ -190,15 +197,15 @@ Segment Segmentation::getLargestSegment_()
 
 void Segmentation::setOutputData(Segment largestSeg)
 {
-    for(int i = extent_[0] ; i <= extent_[1]; i++)
+    LOG_DEBUG("Writing The Output Data");
+    for(int k = extent_[4] ; k <= extent_[5]; k++)
     {
         for(int j = extent_[2] ; j <= extent_[3]; j++)
         {
-            for(int k = extent_[4] ; k <= extent_[5]; k++)
+            for(int i = extent_[0] ; i <= extent_[1]; i++)
             {
                 int ijk[3] = {i, j,k};
                 int segId = pointsLabels_[inputData_->ComputePointId(ijk)];
-                double data = inputData_->GetScalarComponentAsDouble(i, j, k, 0);
                 bool found = false;
                 for(auto it : largestSeg.connectedSegmentsIds_)
                 {
@@ -210,12 +217,20 @@ void Segmentation::setOutputData(Segment largestSeg)
                 }
                 if(largestSeg.connectedSegmentsIds_.contains(segId) || segId == largestSeg.id_ || found)
                 {
-                    outputData_->SetScalarComponentFromDouble(i, j, k, 0, data);
+                    outputData_->SetScalarComponentFromDouble(i,
+                                                              j,
+                                                              k,
+                                                              0,
+                                                              inputData_->GetScalarComponentAsDouble(i, j, k, 0));
                     count_++;
                 }
                 else
                 {
-                    outputData_->SetScalarComponentFromDouble(i, j, k, 0, 0);
+                    outputData_->SetScalarComponentFromDouble(i,
+                                                              j,
+                                                              k,
+                                                              0,
+                                                              0);
                 }
             }
         }
--- mandible-segmentation/src/filters/Segmentation.h
+++ mandible-segmentation/src/filters/Segmentation.h
@@ -13,9 +13,29 @@
 
 struct Segment{
 public:
+
+    /**
+     * @brief id_ : a unique id of the segment
+     */
     int id_;
+
+    /**
+     * @brief pointsCount_
+     * The count of points belongs to this segment.
+     */
     double pointsCount_;
+
+    /**
+     * @brief totalCount_
+     * The count of all points belongs to this
+     * segment including other connected segments
+     */
     double totalCount_;
+
+    /**
+     * @brief connectedSegmentsIds_
+     * A Set of Ids of the equivalent (connected) segments.
+     */
     QSet<int> connectedSegmentsIds_;
 };
 
@@ -26,30 +46,155 @@ public:
     /**
      * @brief Segmentation
      * @param input
+     * vtkImageData source of volume after extracting the VOI (volume of
+     * interest) and thresholding.
      */
     explicit Segmentation(vtkSmartPointer<vtkImageData> input);
 
+    /**
+     * @brief getSegmentedData
+     * Get the result of segmentation
+     * @return
+     * A pointer to the segmented image Data.
+     */
     vtkSmartPointer<vtkImageData> getSegmentedData();
 
 private:
+
+    /**
+     * @brief createOutputData_
+     * Manually create a new image Data to set the output of
+     * segmentation in it.
+     */
     void createOutputData_();
+
+    /**
+     * @brief initializeLabelMap_
+     * Initialize pointsLabels_ map that maps every none zero point to
+     * Its segment.
+     */
     void initializeLabelMap_();
+
+    /**
+     * @brief startLabeling_
+     * Start labeling none zero points of the input data
+     * according to the connectivity with nieghbouring points
+     */
     void startLabeling_();
+
+    /**
+     * @brief setLabel_
+     * @param i
+     * @param j
+     * @param k
+     * Set A label to the point at i, j, and k position
+     * if there is no labled points at nieghbourhood create
+     * a new segment. if there is many different labels
+     * Get Segment of minimum Id and label it with this Segment
+     * and update the corresponding Set of connectedSegmetsIds_.
+     */
     void setLabel_(int i, int j, int k);
-    QList<vtkIdType> getNeighbours_(int i, int j, int k);
+
+    /**
+     * @brief getNeighbours_
+     * Get the neighbours of a specific point at i, j, and k.
+     * @param i
+     * @param j
+     * @param k
+     * @return
+     * A Set of Ids of Segments connected to this point
+     * 13 none zero and connected Points 4 at the same slice
+     * and 9 at the previeous slice.
+     */
+    QSet<int> getNeighbours_(int i, int j, int k);
+
+    /**
+     * @brief createSegment
+     * Create a new segment and insert in in the segmentsList_
+     * @return
+     * the id of the new segment.
+     */
     int createSegment();
+
+    /**
+     * @brief updateSegmentsList
+     * Update the pointsCount, connectedSegmentsIds of the smallest segment id
+     * among the segmentsIds Set.
+     * @param segmentsIds
+     * A Set of segments in the neighbourhood of the point.
+     * @return
+     * Minimum segment id in the segmentsIds set after updating it.
+     */
     int updateSegmentsList(QSet<int> segmentsIds);
+
+    /**
+     * @brief getLargestSegment_
+     * Compute the total count for all segments
+     * this is done over two layers, points of segments
+     * connected to this segment and points connected to
+     * segments of each segment connected to that segment.
+     * and finally optain the segment with maximum points count.
+     * @return
+     * Segment with max points count.
+     */
     Segment getLargestSegment_();
+
+    /**
+     * @brief setOutputData
+     * Set the points connected to largest segment to the  final image.
+     * @param largestSeg
+     */
     void setOutputData(Segment largestSeg);
 
 private:
+
+    /**
+     * @brief inputData_
+     */
     vtkSmartPointer<vtkImageData> inputData_;
+
+    /**
+     * @brief outputData_
+     */
     vtkSmartPointer<vtkImageData> outputData_;
+
+    /**
+     * @brief pointsLabels_
+     * A mapof pointId and segmentId
+     */
     QMap<vtkIdType, int> pointsLabels_;
+
+    /**
+     * @brief segmentsList_
+     * A list of segments provides informations
+     * about the size of each segement and connected
+     * segments
+     */
     QList<Segment>* segmentsList_;
+
+    /**
+     * @brief dimentions_
+     * Dimentions of input volume
+     */
     int* dimentions_;
+
+    /**
+     * @brief extent_
+     * Range of extraced volume in the original volume
+     */
     int* extent_;
+
+    /**
+     * @brief currentId_
+     * Stores the id of the next segment
+     */
     int currentId_;
+
+    /**
+     * @brief count_
+     * Stores the count of points
+     * that actutally have maped to output volume.
+     */
     int count_;
 };
 
