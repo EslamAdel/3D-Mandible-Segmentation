f40fecb18c362afb177b3794c8826b4e09022f0c
eslamadel <eslam.adel.mahmoud.ali@gmail.com>
2016-11-11 11:51:33 +0200

    Calculating execution Time

--- mandible-segmentation/src/apps/develop/segmentation.cpp
+++ mandible-segmentation/src/apps/develop/segmentation.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include <sys/time.h>
 #include <ExtractVOI.h>
 #include <Render3D.h>
 #include <Thresholding.h>
 #include <Segmentation.h>
+#include <stdio.h>
+
 
 vtkImageData *generateTestVolume(int x, int y, int z);
 int main(int argc , char ** argv)
@@ -13,24 +16,48 @@ int main(int argc , char ** argv)
         return EXIT_FAILURE;
     }
 
+    timeval start;
+    timeval end;
+    double segmentationTime;
+    double ThresholdingTime;
+
     Render3D* volRenderer = new Render3D;
+
     volRenderer->loadData(argv[1]);
     volRenderer->rescaleData(volRenderer->getScale(),
                              volRenderer->getShift());
 
-    //    vtkImageData* myTestVolume = generateTestVolume(80, 80, 80);
+
+    vtkImageData* myTestVolume = generateTestVolume(80, 80, 80);
 
     ExtractVOI* extractor = new ExtractVOI;
-
     extractor->setInputData(volRenderer->getShifter()->GetOutputPort());
     extractor->setRange(75, 450, 150, 420, 380, 525);
 
 
+    gettimeofday(&start, NULL);
     Thresholding *thresholdFilter = new Thresholding(extractor->getOutputData(), 1200);
+    gettimeofday(&end, NULL);
+    ThresholdingTime = (end.tv_sec - start.tv_sec) +
+            (end.tv_usec - start.tv_usec)/1000000;
 
+    gettimeofday(&start, NULL);
     Segmentation* mandibleSegmentation = new Segmentation(thresholdFilter->getThresholdedData());
+    gettimeofday(&end, NULL);
+    segmentationTime = (end.tv_sec - start.tv_sec) +
+            (end.tv_usec - start.tv_usec)/1000000;
+
+
+    printf("====================================================== \n");
+    printf("Statistics \n");
+    printf("Thresholding Time : %d min : %f sec \n", (int) ThresholdingTime /60,
+           ThresholdingTime - 60*((int) ThresholdingTime /60));
+    printf("Segmentation Time : %d min : %f sec \n", (int) segmentationTime /60,
+           segmentationTime - 60*((int) segmentationTime /60));
 //    volRenderer->rayCastingRendering(mandibleSegmentation->getSegmentedData());
     volRenderer->extractSurfaces(1300, mandibleSegmentation->getSegmentedData());
+    gettimeofday(&end, NULL);
+
     return EXIT_SUCCESS;
 
 }
