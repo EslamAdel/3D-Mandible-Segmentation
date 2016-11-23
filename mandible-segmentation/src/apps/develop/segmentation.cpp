11c1d06160b04b7a8e38542396d115418b441e16
eslamadel <eslam.adel.mahmoud.ali@gmail.com>
2016-11-23 18:39:06 +0200

    return to mandible example TODO organize names and make another for teeth don't include vtkImageResample if you do mandible then teeth segmentation

--- mandible-segmentation/src/apps/develop/segmentation.cpp
+++ mandible-segmentation/src/apps/develop/segmentation.cpp
@@ -7,7 +7,6 @@
 #include <stdio.h>
 
 
-vtkImageData *generateTestVolume(int x, int y, int z);
 int main(int argc , char ** argv)
 {
     if (argc < 2)
@@ -30,9 +29,6 @@ int main(int argc , char ** argv)
     volRenderer->rescaleData(volRenderer->getScale(),
                              volRenderer->getShift());
 
-
-//    vtkImageData* myTestVolume = generateTestVolume(80, 80, 80);
-
     ExtractVOI* extractor = new ExtractVOI;
     extractor->setInputData(volRenderer->getShifter()->GetOutput());
     extractor->setRange(75, 450, 145, 420, 270, 525);
@@ -50,23 +46,6 @@ int main(int argc , char ** argv)
     msegmentationTime = (end.tv_sec - start.tv_sec) +
             (end.tv_usec - start.tv_usec)/1000000;
 
-    extractor->setInputData(mandibleSegmentation->getSegmentedData());
-    extractor->setRange(75, 450, 145, 420, 390, 525);
-
-    gettimeofday(&start, NULL);
-    Thresholding* teethThresholding = new Thresholding(mandibleSegmentation->getSegmentedData(), 2500);
-    gettimeofday(&end, NULL);
-    tThresholdingTime = (end.tv_sec - start.tv_sec) +
-            (end.tv_usec - start.tv_usec)/1000000;
-
-
-
-    gettimeofday(&start, NULL);
-    Segmentation* teethSegmentation = new Segmentation(teethThresholding->getThresholdedData());
-    gettimeofday(&end, NULL);
-    tsegmentationTime = (end.tv_sec - start.tv_sec) +
-            (end.tv_usec - start.tv_usec)/1000000;
-
     printf("====================================================== \n");
     printf("Statistics \n");
     printf("Mandible Thresholding Time : %d min : %f sec \n", (int) mThresholdingTime /60,
@@ -74,35 +53,9 @@ int main(int argc , char ** argv)
     printf("Mandible Segmentation Time : %d min : %f sec \n", (int) msegmentationTime /60,
            msegmentationTime - 60*((int) msegmentationTime /60));
 
-    printf("Teeth Thresholding Time : %d min : %f sec \n", (int) tThresholdingTime /60,
-           tThresholdingTime - 60*((int) tThresholdingTime /60));
-    printf("Teeth Segmentation Time : %d min : %f sec \n", (int) tsegmentationTime /60,
-           tsegmentationTime - 60*((int) tsegmentationTime /60));
-    //volRenderer->rayCastingRendering(teethSegmentation->getSegmentedData());
-    volRenderer->extractSurfaces(2500, teethSegmentation->getSegmentedData());
+    volRenderer->rayCastingRendering(mandibleSegmentation->getSegmentedData());
     gettimeofday(&end, NULL);
 
     return EXIT_SUCCESS;
 
-}
-
-vtkImageData* generateTestVolume(int x, int y, int z)
-{
-    vtkImageData* testVolume = vtkImageData::New();
-    testVolume->SetDimensions(x, y, z);
-    testVolume->SetScalarType(VTK_UNSIGNED_SHORT, testVolume->GetInformation());
-    testVolume->SetNumberOfScalarComponents(1, testVolume->GetInformation());
-    testVolume->AllocateScalars(VTK_UNSIGNED_SHORT, 1);
-
-    for(int i = 0.25*x; i <  0.75*x; i++)
-    {
-        for(int j = 0.25*y ; j < 0.75*y; j++)
-        {
-            for(int k = 0.25*z ; k <0.75*z ; k++)
-            {
-                testVolume->SetScalarComponentFromDouble(i, j, k, 0, 1500);
-            }
-        }
-    }
-    return testVolume;
 }
