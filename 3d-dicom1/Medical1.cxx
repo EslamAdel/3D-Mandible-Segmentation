7597695ae7ce008b22d7ae0edc2e092fd68fe83f
eslamadel <eslam.adel.mahmoud.ali@gmail.com>
2018-06-23 17:02:30 +0200

    Sking and bone

--- 3d-dicom1/Medical1.cxx
+++ 3d-dicom1/Medical1.cxx
@@ -40,49 +40,62 @@
 #include <vtkDICOMImageReader.h>
 #include <vtkMetaImageReader.h>
 #include <vtkSmoothPolyDataFilter.h>
+#include <vtkImageShiftScale.h>
 
 
 int main (int argc, char *argv[])
 {
-  if (argc < 2)
+    if (argc < 2)
     {
-    cout << "Usage: " << argv[0] << " DATADIR/headsq/quarter" << endl;
-    return EXIT_FAILURE;
+        cout << "Usage: " << argv[0] << " DATADIR/headsq/quarter" << endl;
+        return EXIT_FAILURE;
     }
 
-  // Create the renderer, the render window, and the interactor. The
-  // renderer draws into the render window, the interactor enables
-  // mouse- and keyboard-based interaction with the data within the
-  // render window.
-  //
-  vtkSmartPointer<vtkRenderer> aRenderer = vtkSmartPointer<vtkRenderer>::New();
-  vtkSmartPointer<vtkRenderWindow> renWin =
-    vtkSmartPointer<vtkRenderWindow>::New();
-  renWin->AddRenderer(aRenderer);
-  vtkSmartPointer<vtkRenderWindowInteractor> iren =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  iren->SetRenderWindow(renWin);
+    // Create the renderer, the render window, and the interactor. The
+    // renderer draws into the render window, the interactor enables
+    // mouse- and keyboard-based interaction with the data within the
+    // render window.
+    //
+    vtkSmartPointer<vtkRenderer> aRenderer = vtkSmartPointer<vtkRenderer>::New();
+    vtkSmartPointer<vtkRenderWindow> renWin =
+            vtkSmartPointer<vtkRenderWindow>::New();
+    renWin->AddRenderer(aRenderer);
+    vtkSmartPointer<vtkRenderWindowInteractor> iren =
+            vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    iren->SetRenderWindow(renWin);
 
-  // Set a background color for the renderer and set the size of the
-  // render window (expressed in pixels).
-  aRenderer->SetBackground(.2, .3, .4);
-  renWin->SetSize(640, 480);
 
-  // The following reader is used to read a series of 2D slices (images)
-  // that compose the volume. The slice dimensions are set, and the
-  // pixel spacing. The data Endianness must also be specified. The
-  // reader uses the FilePrefix in combination with the slice number to
-  // construct filenames using the format FilePrefix.%d. (In this case
-  // the FilePrefix is the root name of the file: quarter.)
 
-   //std::string folder = argv[1];
+    // Set a background color for the renderer and set the size of the
+    // render window (expressed in pixels).
+    aRenderer->SetBackground(.2, .3, .4);
+    renWin->SetSize(640, 480);
 
-/*
-  vtkSmartPointer<vtkDICOMImageReader> v16 =
-      vtkSmartPointer<vtkDICOMImageReader>::New();
-   v16->SetDirectoryName(folder.c_str());
-   v16->Update();
-*/
+    // The following reader is used to read a series of 2D slices (images)
+    // that compose the volume. The slice dimensions are set, and the
+    // pixel spacing. The data Endianness must also be specified. The
+    // reader uses the FilePrefix in combination with the slice number to
+    // construct filenames using the format FilePrefix.%d. (In this case
+    // the FilePrefix is the root name of the file: quarter.)
+
+    std::string folder = argv[1];
+
+
+    vtkSmartPointer<vtkDICOMImageReader> v16 =
+            vtkSmartPointer<vtkDICOMImageReader>::New();
+    v16->SetDirectoryName(folder.c_str());
+    v16->Update();
+
+    float scale    = v16->GetRescaleSlope();
+    float shift   = -1 * v16->GetRescaleOffset();
+
+    vtkSmartPointer<vtkImageShiftScale> shifter = vtkSmartPointer<vtkImageShiftScale>::New();
+    shifter->SetScale(scale);
+    shifter->SetShift(shift);
+    shifter->SetOutputScalarTypeToUnsignedShort();
+    shifter->SetInputData(v16->GetOutput());
+    shifter->Update();
+    /*
      vtkMetaImageReader *v16 = vtkMetaImageReader::New();
      v16->SetFileName(argv[1]);
      v16->Update();
@@ -96,76 +109,76 @@ int main (int argc, char *argv[])
   v16->SetDataSpacing (3.2, 3.2, 1.5);
   v16->Update();
 */
-  // An isosurface, or contour value of 500 is known to correspond to
-  // the skin of the patient. Once generated, a vtkPolyDataNormals
-  // filter is is used to create normals for smooth surface shading
-  // during rendering.  The triangle stripper is used to create triangle
-  // strips from the isosurface; these render much faster on may
-  // systems.
-  vtkSmartPointer<vtkContourFilter> skinExtractor =
-    vtkSmartPointer<vtkContourFilter>::New();
-  skinExtractor->SetInputConnection( v16->GetOutputPort());
-  skinExtractor->SetValue(0, -500);
-  skinExtractor->Update();
+    // An isosurface, or contour value of 500 is known to correspond to
+    // the skin of the patient. Once generated, a vtkPolyDataNormals
+    // filter is is used to create normals for smooth surface shading
+    // during rendering.  The triangle stripper is used to create triangle
+    // strips from the isosurface; these render much faster on may
+    // systems.
+    vtkSmartPointer<vtkContourFilter> skinExtractor =
+            vtkSmartPointer<vtkContourFilter>::New();
+    skinExtractor->SetInputConnection( shifter->GetOutputPort());
+    skinExtractor->SetValue(0, 500);
+    skinExtractor->Update();
 
 
-  vtkSmartPointer<vtkPolyDataNormals> skinNormals =
-    vtkSmartPointer<vtkPolyDataNormals>::New();
-  skinNormals->SetInputConnection(skinExtractor->GetOutputPort());
-  skinNormals->SetFeatureAngle(60.0);
-  skinNormals->Update();
+    vtkSmartPointer<vtkPolyDataNormals> skinNormals =
+            vtkSmartPointer<vtkPolyDataNormals>::New();
+    skinNormals->SetInputConnection(skinExtractor->GetOutputPort());
+    skinNormals->SetFeatureAngle(60.0);
+    skinNormals->Update();
 
-  vtkSmartPointer<vtkStripper> skinStripper =
-    vtkSmartPointer<vtkStripper>::New();
-  skinStripper->SetInputConnection(skinNormals->GetOutputPort());
-  skinStripper->Update();
+    vtkSmartPointer<vtkStripper> skinStripper =
+            vtkSmartPointer<vtkStripper>::New();
+    skinStripper->SetInputConnection(skinNormals->GetOutputPort());
+    skinStripper->Update();
 
-  vtkSmartPointer<vtkPolyDataMapper> skinMapper =
-    vtkSmartPointer<vtkPolyDataMapper>::New();
-  skinMapper->SetInputConnection(skinStripper->GetOutputPort());
-  skinMapper->ScalarVisibilityOff();
+    vtkSmartPointer<vtkPolyDataMapper> skinMapper =
+            vtkSmartPointer<vtkPolyDataMapper>::New();
+    skinMapper->SetInputConnection(skinStripper->GetOutputPort());
+    skinMapper->ScalarVisibilityOff();
 
-  vtkSmartPointer<vtkActor> skin =
-    vtkSmartPointer<vtkActor>::New();
-  skin->SetMapper(skinMapper);
-  skin->GetProperty()->SetDiffuseColor(1, .49, .25);
-  skin->GetProperty()->SetSpecular(.3);
-  skin->GetProperty()->SetSpecularPower(20);
+    vtkSmartPointer<vtkActor> skin =
+            vtkSmartPointer<vtkActor>::New();
+    skin->SetMapper(skinMapper);
+    skin->GetProperty()->SetDiffuseColor(1, .49, .25);
+    skin->GetProperty()->SetSpecular(.3);
+    skin->GetProperty()->SetSpecularPower(20);
 
-  // An isosurface, or contour value of 1150 is known to correspond to
-  // the skin of the patient. Once generated, a vtkPolyDataNormals
-  // filter is is used to create normals for smooth surface shading
-  // during rendering.  The triangle stripper is used to create triangle
-  // strips from the isosurface; these render much faster on may
-  // systems.
-  vtkSmartPointer<vtkContourFilter> boneExtractor =
-    vtkSmartPointer<vtkContourFilter>::New();
-  boneExtractor->SetInputConnection(v16->GetOutputPort());
-  boneExtractor->SetValue(0, 1200);
-  vtkSmoothPolyDataFilter* smoother =vtkSmoothPolyDataFilter::New();
-  smoother->SetInputConnection(boneExtractor->GetOutputPort());
-  smoother->SetNumberOfIterations(600);
+    // An isosurface, or contour value of 1150 is known to correspond to
+    // the skin of the patient. Once generated, a vtkPolyDataNormals
+    // filter is is used to create normals for smooth surface shading
+    // during rendering.  The triangle stripper is used to create triangle
+    // strips from the isosurface; these render much faster on may
+    // systems.
+    vtkSmartPointer<vtkContourFilter> boneExtractor =
+            vtkSmartPointer<vtkContourFilter>::New();
+    boneExtractor->SetInputConnection(shifter->GetOutputPort());
+    boneExtractor->SetValue(0, 1000);
+//    vtkSmoothPolyDataFilter* smoother =vtkSmoothPolyDataFilter::New();
+//    smoother->SetInputConnection(boneExtractor->GetOutputPort());
+//    smoother->SetNumberOfIterations(2);
 
 
-  vtkSmartPointer<vtkPolyDataNormals> boneNormals =
-    vtkSmartPointer<vtkPolyDataNormals>::New();
-  boneNormals->SetInputConnection(smoother->GetOutputPort());
-  boneNormals->SetFeatureAngle(60.0);
+    vtkSmartPointer<vtkPolyDataNormals> boneNormals =
+            vtkSmartPointer<vtkPolyDataNormals>::New();
+    boneNormals->SetInputConnection(boneExtractor->GetOutputPort());
+    boneNormals->SetFeatureAngle(60.0);
 
-  vtkSmartPointer<vtkStripper> boneStripper =
-    vtkSmartPointer<vtkStripper>::New();
-  boneStripper->SetInputConnection(boneNormals->GetOutputPort());
+    vtkSmartPointer<vtkStripper> boneStripper =
+            vtkSmartPointer<vtkStripper>::New();
+    boneStripper->SetInputConnection(boneNormals->GetOutputPort());
 
-  vtkSmartPointer<vtkPolyDataMapper> boneMapper =
-    vtkSmartPointer<vtkPolyDataMapper>::New();
-  boneMapper->SetInputConnection(boneStripper->GetOutputPort());
-  boneMapper->ScalarVisibilityOff();
+    vtkSmartPointer<vtkPolyDataMapper> boneMapper =
+            vtkSmartPointer<vtkPolyDataMapper>::New();
+    boneMapper->SetInputConnection(boneStripper->GetOutputPort());
+    boneMapper->ScalarVisibilityOff();
 
-  vtkSmartPointer<vtkActor> bone =
-    vtkSmartPointer<vtkActor>::New();
-  bone->SetMapper(boneMapper);
-  bone->GetProperty()->SetDiffuseColor(1, 1, .9412);
-/*
+    vtkSmartPointer<vtkActor> bone =
+            vtkSmartPointer<vtkActor>::New();
+    bone->SetMapper(boneMapper);
+    bone->GetProperty()->SetDiffuseColor(1, 1, .9412);
+    /*
   // An outline provides context around the data.
   //
   vtkSmartPointer<vtkOutlineFilter> outlineData =
@@ -260,55 +273,55 @@ int main (int argc, char *argv[])
   coronal->GetMapper()->SetInputConnection(coronalColors->GetOutputPort());
   coronal->SetDisplayExtent(0,63, 32,32, 0,92);
 */
-  // It is convenient to create an initial view of the data. The
-  // FocalPoint and Position form a vector direction. Later on
-  // (ResetCamera() method) this vector is used to position the camera
-  // to look at the data in this direction.
-  vtkSmartPointer<vtkCamera> aCamera =
-    vtkSmartPointer<vtkCamera>::New();
-  aCamera->SetViewUp (0, 0, -1);
-  aCamera->SetPosition (0, 1, 0);
-  aCamera->SetFocalPoint (0, 0, 0);
-  aCamera->ComputeViewPlaneNormal();
-  aCamera->Azimuth(30.0);
-  aCamera->Elevation(30.0);
+    // It is convenient to create an initial view of the data. The
+    // FocalPoint and Position form a vector direction. Later on
+    // (ResetCamera() method) this vector is used to position the camera
+    // to look at the data in this direction.
+    vtkSmartPointer<vtkCamera> aCamera =
+            vtkSmartPointer<vtkCamera>::New();
+    aCamera->SetViewUp (0, 0, -1);
+    aCamera->SetPosition (0, 1, 0);
+    aCamera->SetFocalPoint (0, 0, 0);
+    aCamera->ComputeViewPlaneNormal();
+    aCamera->Azimuth(30.0);
+    aCamera->Elevation(30.0);
 
-  // Actors are added to the renderer.
-//  aRenderer->AddActor(outline);
-//  aRenderer->AddActor(sagittal);
-//  aRenderer->AddActor(axial);
-//  aRenderer->AddActor(coronal);
-//  aRenderer->AddActor(skin);
-  aRenderer->AddActor(bone);
+    // Actors are added to the renderer.
+    //  aRenderer->AddActor(outline);
+    //  aRenderer->AddActor(sagittal);
+    //  aRenderer->AddActor(axial);
+    //  aRenderer->AddActor(coronal);
+    aRenderer->AddActor(skin);
+    aRenderer->AddActor(bone);
 
-  // Turn off bone for this example.
-  //bone->VisibilityOff();
+    // Turn off bone for this example.
+    //bone->VisibilityOff();
 
-  // Set skin to semi-transparent.
-  //skin->GetProperty()->SetOpacity(0.8);
+    // Set skin to semi-transparent.
+    //skin->GetProperty()->SetOpacity(0.8);
 
-  // An initial camera view is created.  The Dolly() method moves
-  // the camera towards the FocalPoint, thereby enlarging the image.
-  aRenderer->SetActiveCamera(aCamera);
+    // An initial camera view is created.  The Dolly() method moves
+    // the camera towards the FocalPoint, thereby enlarging the image.
+    aRenderer->SetActiveCamera(aCamera);
 
-  // Calling Render() directly on a vtkRenderer is strictly forbidden.
-  // Only calling Render() on the vtkRenderWindow is a valid call.
-  renWin->Render();
+    // Calling Render() directly on a vtkRenderer is strictly forbidden.
+    // Only calling Render() on the vtkRenderWindow is a valid call.
+    renWin->Render();
 
-  aRenderer->ResetCamera ();
-  aCamera->Dolly(1.5);
+    aRenderer->ResetCamera ();
+    aCamera->Dolly(1.5);
 
-  // Note that when camera movement occurs (as it does in the Dolly()
-  // method), the clipping planes often need adjusting. Clipping planes
-  // consist of two planes: near and far along the view direction. The
-  // near plane clips out objects in front of the plane; the far plane
-  // clips out objects behind the plane. This way only what is drawn
-  // between the planes is actually rendered.
-  aRenderer->ResetCameraClippingRange ();
+    // Note that when camera movement occurs (as it does in the Dolly()
+    // method), the clipping planes often need adjusting. Clipping planes
+    // consist of two planes: near and far along the view direction. The
+    // near plane clips out objects in front of the plane; the far plane
+    // clips out objects behind the plane. This way only what is drawn
+    // between the planes is actually rendered.
+    aRenderer->ResetCameraClippingRange ();
 
-  // interact with data
-  iren->Initialize();
-  iren->Start();
+    // interact with data
+    iren->Initialize();
+    iren->Start();
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
